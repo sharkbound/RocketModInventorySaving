1f85d0a2eb41864f5f1b6f358d112da364257069
sharkbound <laptopblaster@gmail.com>
2016-08-14 20:46:54 -0700

    First Commit

---
+++ InventorySaving/InventorySaving/CommandInv.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Unturned.Player;
+using Rocket.API;
+using Rocket.Core;
+using Rocket.Unturned.Chat;
+
+namespace InventorySaving
+{
+    class CommandInv : IRocketCommand
+    {
+        Weapons Weapons = new Weapons();
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get { return Rocket.API.AllowedCaller.Player; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 1)
+            {
+                UnturnedChat.Say(caller, "Incorrect command usage, Correct usage: /inv [ save | load ]");
+                return;
+            }
+
+            UnturnedPlayer Player = (UnturnedPlayer)caller;
+            if (command[0].ToLower() == "save")
+            {
+                Weapons.SaveWeaponData(Player); 
+            }
+            else if (command[0].ToLower() == "load")
+            {
+                if (Weapons.ContainsWeaponData(Player))
+                {
+                    if (Plugin.SavedWeapons[Player.CSteamID].Slot1 != null)
+                    {
+                        Player.Inventory.tryAddItem(Plugin.SavedWeapons[Player.CSteamID].Slot1, true);
+                    }
+                    if (Plugin.SavedWeapons[Player.CSteamID].Slot2 != null)
+                    {
+                        Player.Inventory.tryAddItem(Plugin.SavedWeapons[Player.CSteamID].Slot2, true);
+                    }
+
+                    UnturnedChat.Say(caller, "Your inventory has been restored!");
+                }
+                else
+                {
+                    UnturnedChat.Say(caller, "You have not done /inv save yet!");
+                }
+            }
+        }
+
+        public string Help
+        {
+            get { return "Saves/loads your inventory"; }
+        }
+
+        public string Name
+        {
+            get { return "inv"; }
+        }
+
+        public List<string> Permissions
+        {
+            get { return new List<string> { "inv" }; }
+        }
+
+        public string Syntax
+        {
+            get { return "[ save|load ]"; }
+        }
+    }
+}
---
+++ InventorySaving/InventorySaving/Plugin.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Player;
+using Steamworks;
+using SDG.Unturned;
+using Rocket.Core.Plugins;
+
+namespace InventorySaving
+{
+    class Plugin : RocketPlugin<Config>
+    {
+        public static Dictionary<CSteamID, Weapons> SavedWeapons = new Dictionary<CSteamID, Weapons>();
+
+        protected override void Load()
+        {
+            Logger.Log("InventorySaving has loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("InventorySaving has unloaded!");
+        }
+    }
+
+    public class Weapons
+    {
+        public Item Slot1 = null;
+        public Item Slot2 = null;
+        ItemJar slot1;
+        ItemJar slot2;
+
+        public void SaveWeaponData(UnturnedPlayer player)
+        {
+            Weapons w = new Weapons();
+            w.slot1 = player.Player.inventory.getItem(0, 0);
+            w.slot2 = player.Player.inventory.getItem(1, 0);
+            w.Slot1 = new Item(slot1.item.ItemID, 1, 100, slot1.Item.Metadata);
+            w.Slot2 = new Item(slot2.item.ItemID, 1, 100, slot2.Item.Metadata);
+
+            if (!Plugin.SavedWeapons.ContainsKey(player.CSteamID))
+            {
+                Plugin.SavedWeapons.Add(player.CSteamID, w);
+            }
+            else
+            {
+                Plugin.SavedWeapons[player.CSteamID] = w;
+            }
+        }
+
+        public bool ContainsWeaponData(UnturnedPlayer player)
+        {
+            if (!Plugin.SavedWeapons.ContainsKey(player.CSteamID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /*
+            Unturned weapon metadata structure.
+            metadata[0] = sight id byte 1
+            metadata[1] = sight id byte 2
+            metadata[2] = tactical id byte 1
+            metadata[3] = tactical id byte 2
+            
+            metadata[4] = grip id byte 1
+            metadata[5] = grip id byte 2
+            metadata[6] = barrel id byte 1
+            metadata[7] = barrel id byte 2
+            metadata[8] = magazine id byte 1
+            metadata[9] = magazine id byte 2
+            metadata[10] = ammo
+            metadata[11] = firemode
+            metadata[12] = ??
+            metadata[13] = sight durability
+            metadata[14] = tactical durability
+            metadata[15] = grip durability
+            metadata[16] = barrel durability
+            metadata[17] = magazine durability
+        */
+    }
+}
